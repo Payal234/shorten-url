9c4d99039de648837ef1bbd13ac1ce6097ee819d
Payal234 <payaldhobale399@gmail.com>
2025-09-07 14:09:45 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
+  images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 't3.ftcdn.net',
         port: '',
-        
       },
     ],
+  },
+  env: {
+    MONGODB_URI: process.env.MONGODB_URI,
   },
 };
 
